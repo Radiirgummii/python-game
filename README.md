804ced15ce3636e856d1adae9dcae6de87990555
Radiirgummii <git@radiirgummii.net>
2023-03-20 13:25:31 -0700

    completed

---
+++ README.md
@@ -0,0 +1,21 @@
+# 4 Gewinnt
+## Spielidee
+Dieses Spiel ist ein 4 gewinnt Spiel, bei dem zwei Spieler gegeneinander antreten. 
+Als Spieler kann man Steine in eine der sieben Spalten einfügen, um eine Reihe von vier Steinen zu bilden. 
+Der Spieler, der als erster vier Steine in einer waagerechten, senkrechten oder diagonalen Linie hat, gewinnt.
+## Regeln
+- Zwei Spieler treten gegeneinander an
+- Spielfeld ist 7 mal 6
+- Spieler fügen Steine in eine der 7 Spalten ein
+- Gewinner ist, wer als erster 4 Steine in einer waagerechten, senkrechten oder diagonalen Linie hat
+## Projektplan
+- Regeln und Spielidee verfassen
+- Projektplan Schreiben
+- grundlegende Spiellogik implementieren
+    - Board
+    - Züge mit einfacher Eingabe
+    - keine illegalen Züge
+    - erkennen ob Spieler gewonnen hat
+    - Logik zusammenführen
+    - Auf Bugs testen
+- Präsentation vorbereiten
--- main.py
+++ main.py
@@ -1,27 +1,128 @@
-from ursina import Ursina,Entity,color # type: ignore
-from ursina.prefabs.platformer_controller_2d import PlatformerController2d# type: ignore
-import json
+import colorama  # type: ignore
+from playsound import playsound # type: ignore
+colorama.just_fix_windows_console()
 
-def loadlevel(level):
-    for x,row in zip(range(4,-4,-0.5),level["map"]):
-        for y, block in zip(range(4,-4,-0.5),row):
-            if block != 0:
-                Entity(model='quad', scale=(0.5, 0.5), collider='box', color=color.black,y=y,x=x)
+# Funktion gibt den Inhalt einer Zelle des Spielfeldes zurück, wenn die Zelle innerhalb des Spielfeldes liegt
 
 
+def get(matrix, x, y):
 
-with open("assets/levels.json") as f:
-    levels = json.load(f)
+    try:
+        if x < 0 or y < 0:
+            return False
+        return matrix[y][x]
+    except:
+        return False
+
+# Klasse Board erstellt das Spielfeld und verwaltet die Spielregeln
 
 
-app = Ursina()
+class Board:
+
+    # Konstruktor der Klasse Board
+    def __init__(self, players=[colorama.Fore.RED+"⬤"+colorama.Style.RESET_ALL, colorama.Fore.BLUE+"⬤"+colorama.Style.RESET_ALL]):
+        # Erstellt ein leeres Spielfeld
+        self.board = [[" ", " ", " ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " ", " ", " "],
+                      [" ", " ", " ", " ", " ", " ", " "]]
+        # Speichert die Spieler
+        self.players = players
+        # speichert die Gewinnmuster
+        self.winpatterns = [[[-3, 0], [-2, 0], [-1, 0], [0, 0]],
+                            [[-2, 0], [-1, 0], [0, 0], [1, 0]],
+                            [[-1, -1], [0, 0], [1, 1], [2, 2]],
+                            [[0, 0], [1, 1], [2, 2], [3, 3]],
+                            [[3, 0], [2, 0], [1, 0], [0, 0]],
+                            [[2, 0], [1, 0], [0, 0], [-1, 0]],
+                            [[1, -1], [0, 0], [-1, 1], [-2, 2]],
+                            [[0, 0], [-1, 1], [-2, 2], [-3, 3]],
+                            [[-3, 0], [-2, 0], [-1, 0], [0, 0]],
+                            [[-2, 0], [-1, 0], [0, 0], [1, 0]],
+                            [[-1, 1], [0, 0], [1, -1], [2, -2]],
+                            [[0, 0], [1, -1], [2, -2], [3, -3]],
+                            [[3, 0], [2, 0], [1, 0], [0, 0]],
+                            [[2, 0], [1, 0], [0, 0], [-1, 0]],
+                            [[1, 1], [0, 0], [-1, -1], [-2, -2]],
+                            [[0, 0], [-1, -1], [-2, -2], [-3, -3]],
+                            [[0, 0], [0, 1], [0, 2], [0, 3]],
+                            [[0, 0], [0, -1], [0, -2], [0, -3]],
+                            [[0, -1], [0, 0], [0, 1], [0, 2]],
+                            [[0, 1], [0, 0], [0, -1], [0, -2]]]
+
+    # Funktion fügt einen Spielstein in das Spielfeld ein
+    def play(self, player: str, row: int):
+        # Das Spielfeld wird umgedreht, da die Spielsteine bei play() von oben nach unten eingefügt werden
+        self.board.reverse()
+        # Schleife, die durch das Spielfeld geht und den Spielstein an die erste freie Stelle setzt
+        for i, c in enumerate(self.board):
+            if c[row] == " ":
+                c[row] = player
+                break
+        # Umgedrehtes Spielfeld wird wieder in den Ursprungszustand gebracht
+        self.board.reverse()
+        # Koordinaten des Spielsteins werden zurückgegeben
+        return [row, len(self.board)-i-1]
+
+    # Funktion überprüft, ob ein Spieler gewonnen hat
+    def check_win(self, cords: list):
+        # Schleife, die durch alle Gewinnmuster geht und überprüft, ob 4 Steine in einer Reihe liegen
+        for i in self.winpatterns:
+            if get(self.board, cords[0] + i[0][0], cords[1] + i[0][1]) == get(self.board, cords[0] + i[1][0], cords[1] + i[1][1]) == get(self.board, cords[0] + i[2][0], cords[1] + i[2][1]) == get(self.board, cords[0] + i[3][0], cords[1] + i[3][1]) != " ":
+                # Wenn 4 Steine in einer Reihe liegen, wird der Inhalt der Zelle (Spieler) zurückgegeben
+                return get(self.board, cords[0] + i[0][0], cords[1] + i[0][1])
+
+    # Funktion gibt das Spielfeld als String zurück
+
+    def __str__(self):
+        string = "  1   2   3   4   5   6   7"
+        for i in self.board:
+            string += "\n" + "--" * len(self.board[0]) * 2 + "-\n|"
+            for j in i:
+                string += f" {j} |"
+
+        return string
 
 
-player = PlatformerController2d(y=1, z=.01, scale=0.5, max_jumps=2, jump_height=1, position = (-4, 0))
-Ramen = [Entity(model='quad', scale=(9, 0.5), collider='box', color=color.red,y=-4.5),
-         Entity(model='quad', scale=(0.5, 9), collider='box', color=color.red,x=-4.5),
-         Entity(model='quad', scale=(0.5, 9), collider='box', color=color.red,x=4.5),
-         Entity(model='quad', scale=(9, 0.5), collider='box', color=color.red,y=4.5)]
-loadlevel(levels["1"])
+# Funktion, die die Hauptschleife des Spiels enthält
+def main():
 
-app.run()
+    # Spielfeld wird erstellt
+    board = Board()
+    # Endlosschleife, die durch die Spieler geht und für jeden eine Eingabe abfragt
+    while True:
+        for player in board.players:
+            print(board)
+            # Spielstein wird an einer Spalte eingefügt. wenn der Spieler keine Zahl zwischen 1 und 7 eigibt, wird erneut abgefragt.
+            while True:
+                try:
+                    col = input(f"{player} pick column: ")
+                    if col == "Blutwurstblase":
+                        board = Board()
+                        break
+                    elif col == "":
+                        playsound("assets/easteregg.mp3")
+                    if 0 < int(col) < 8:
+                        break
+                    print("invalid input(valid: 1-7)")
+                except ValueError:
+                    print("invalid input(valid: 1-7)")
+
+            cords = board.play(player, int(col) - 1)
+            # Gewinner wird überprüft
+            winner = board.check_win(cords)
+            if winner:
+
+                # Wenn ein Gewinner gefunden wurde, wird das Spielfeld ausgegeben und der Gewinner angezeigt
+                print(board)
+                print(f"{winner} won!")
+                return
+            # Spielfeld wird ausgegeben
+            
+
+
+# Prüft, ob das Programm direkt gestartet wurde
+if __name__ == "__main__":
+    main()
